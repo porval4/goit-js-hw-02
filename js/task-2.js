01b738eba08e9bc51f7e28ffb6737f400becbb25
porval4 <133165484+porval4@users.noreply.github.com>
2024-01-10 01:05:01 +0200

    save 2

---
+++ js/task-2.js
@@ -0,0 +1,16 @@
+function formatMessage(message, maxLength) {
+    if (message.length <= maxLength) {
+        return message;
+    } else {
+        // Обрізка рядка до maxLength символів та додавання трьох крапок
+        return message.slice(0, maxLength) + "...";
+    }
+}
+
+// Перевірка роботи функції з різними вхідними параметрами
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
